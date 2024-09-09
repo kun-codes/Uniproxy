7527d80485260d45392521d8256fdd8ea46c74eb
kun-codes <77796630+throwaway69420-69420@users.noreply.github.com>
2024-09-10 00:08:26 +0530

    refactor(shell_env_var): refactor to remove duplicate functions and lessen indentation

--- proxverter/shell_env_var.py
+++ proxverter/shell_env_var.py
@@ -44,79 +44,68 @@ class ShellEnvVar:
     def command_exists(self, command):
         return subprocess.call(["which", command], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) == 0
 
-    def enable_proxy(self):
+
+    def set_proxy_env_var(self):
         for shell in self.shells:
-            if shell["installed"]:
-                self.write_proxy_env_var(shell["name"], shell["config"])
-                self.write_bypass_domains_env_var(shell["name"], shell["config"])
+            if not shell["installed"]:
+                continue
 
-    def disable_proxy(self):
+            shell_rc = shell["config"]
+            if not os.path.exists(shell_rc):
+                open(shell_rc, 'w').close()
+
+            if shell["name"] in ["BASH", "ZSH"]:
+                with open(shell_rc, 'a') as f:
+                    f.write(f'export http_proxy="http://{self.ip_address}:{self.port}/"\n')
+                    f.write(f'export HTTP_PROXY="http://{self.ip_address}:{self.port}/"\n')
+                    f.write(f'export https_proxy="https://{self.ip_address}:{self.port}/"\n')
+                    f.write(f'export HTTPS_PROXY="https://{self.ip_address}:{self.port}/"\n')
+                    f.write(f'export ftp_proxy="ftp://{self.ip_address}:{self.port}/"\n')
+                    f.write(f'export FTP_PROXY="ftp://{self.ip_address}:{self.port}/"\n')
+                    f.write(f'export rsync_proxy="rsync://{self.ip_address}:{self.port}/"\n')
+                    f.write(f'export RSYNC_PROXY="rsync://{self.ip_address}:{self.port}/"\n')
+
+            elif shell["name"] == "FISH":
+                with open(shell_rc, 'a') as f:
+                    f.write(f'set -x http_proxy "http://{self.ip_address}:{self.port}/"\n')
+                    f.write(f'set -x HTTP_PROXY "http://{self.ip_address}:{self.port}/"\n')
+                    f.write(f'set -x https_proxy "https://{self.ip_address}:{self.port}/"\n')
+                    f.write(f'set -x HTTPS_PROXY "https://{self.ip_address}:{self.port}/"\n')
+                    f.write(f'set -x ftp_proxy "ftp://{self.ip_address}:{self.port}/"\n')
+                    f.write(f'set -x FTP_PROXY "ftp://{self.ip_address}:{self.port}/"\n')
+                    f.write(f'set -x rsync_proxy "rsync://{self.ip_address}:{self.port}/"\n')
+                    f.write(f'set -x RSYNC_PROXY "rsync://{self.ip_address}:{self.port}/"\n')
+
+    def unset_proxy_env_var(self):
         for shell in self.shells:
-            if shell["installed"]:
-                self.del_proxy_env_var(shell["name"], shell["config"])
-                self.del_bypass_domains_env_var(shell["name"], shell["config"])
+            if not shell["installed"]:
+                continue
 
-    def set_bypass_domains(self):
-        for shell in self.shells:
-            if shell["installed"]:
-                self.write_bypass_domains_env_var(shell["name"], shell["config"])
+            shell_rc = shell["config"]
+            if not os.path.exists(shell_rc):
+                continue
 
-    def unset_bypass_domains(self):
-        for shell in self.shells:
-            if shell["installed"]:
-                self.del_bypass_domains_env_var(shell["name"], shell["config"])
+            with open(shell_rc, 'r') as f:
+                lines = f.readlines()
 
-    def write_proxy_env_var(self, shell_name, shell_rc):
-        if not os.path.exists(shell_rc):
-            with open(shell_rc, 'w') as f:
-                pass
+            proxy_vars = [
+                "http_proxy",
+                "HTTP_PROXY",
+                "https_proxy",
+                "HTTPS_PROXY",
+                "ftp_proxy",
+                "FTP_PROXY",
+                "rsync_proxy",
+                "RSYNC_PROXY"
+            ]
 
-        if shell_name in ["BASH", "ZSH"]:
-            with open(shell_rc, 'a') as f:
-                f.write(f'export http_proxy="http://{self.ip_address}:{self.port}/"\n')
-                f.write(f'export HTTP_PROXY="http://{self.ip_address}:{self.port}/"\n')
-                f.write(f'export https_proxy="https://{self.ip_address}:{self.port}/"\n')
-                f.write(f'export HTTPS_PROXY="https://{self.ip_address}:{self.port}/"\n')
-                f.write(f'export ftp_proxy="ftp://{self.ip_address}:{self.port}/"\n')
-                f.write(f'export FTP_PROXY="ftp://{self.ip_address}:{self.port}/"\n')
-                f.write(f'export rsync_proxy="rsync://{self.ip_address}:{self.port}/"\n')
-                f.write(f'export RSYNC_PROXY="rsync://{self.ip_address}:{self.port}/"\n')
-        elif shell_name == "FISH":
-            with open(shell_rc, 'a') as f:
-                f.write(f'set -x http_proxy "http://{self.ip_address}:{self.port}/"\n')
-                f.write(f'set -x HTTP_PROXY "http://{self.ip_address}:{self.port}/"\n')
-                f.write(f'set -x https_proxy "https://{self.ip_address}:{self.port}/"\n')
-                f.write(f'set -x HTTPS_PROXY "https://{self.ip_address}:{self.port}/"\n')
-                f.write(f'set -x ftp_proxy "ftp://{self.ip_address}:{self.port}/"\n')
-                f.write(f'set -x FTP_PROXY "ftp://{self.ip_address}:{self.port}/"\n')
-                f.write(f'set -x rsync_proxy "rsync://{self.ip_address}:{self.port}/"\n')
-                f.write(f'set -x RSYNC_PROXY "rsync://{self.ip_address}:{self.port}/"\n')
+            proxy_patterns_bash_zsh = [re.compile(rf'export {var}\=.*') for var in proxy_vars]
+            proxy_patterns_fish = [re.compile(rf'set -x {var} .*') for var in proxy_vars]
 
-    def del_proxy_env_var(self, shell_name, shell_rc):
-        if not os.path.exists(shell_rc):
-            return
-
-        with open(shell_rc, 'r') as f:
-            lines = f.readlines()
-
-        proxy_vars = [
-            "http_proxy",
-            "HTTP_PROXY",
-            "https_proxy",
-            "HTTPS_PROXY",
-            "ftp_proxy",
-            "FTP_PROXY",
-            "rsync_proxy",
-            "RSYNC_PROXY"
-        ]
-
-        proxy_patterns_bash_zsh = [re.compile(rf'export {var}\=.*') for var in proxy_vars]
-        proxy_patterns_fish = [re.compile(rf'set -x {var} .*') for var in proxy_vars]
-
-        if shell_name in ["BASH", "ZSH"]:
-            self.remove_matching_lines(lines, proxy_patterns_bash_zsh, shell_rc)
-        elif shell_name == "FISH":
-            self.remove_matching_lines(lines, proxy_patterns_fish, shell_rc)
+            if shell["name"] in ["BASH", "ZSH"]:
+                self.remove_matching_lines(lines, proxy_patterns_bash_zsh, shell_rc)
+            elif shell["name"] == "FISH":
+                self.remove_matching_lines(lines, proxy_patterns_fish, shell_rc)
 
     def remove_matching_lines(self, lines, patterns, shell_rc):
         try:
@@ -127,38 +116,47 @@ class ShellEnvVar:
         except FileNotFoundError:
             pass
 
-    def write_bypass_domains_env_var(self, shell_name, shell_rc):
-        if not os.path.exists(shell_rc):
-            with open(shell_rc, 'w') as f:
-                pass
+    def set_bypass_domains_env_var(self):
+        for shell in self.shells:
+            if not shell["installed"]:
+                continue
 
-        bypass_domains_str = ",".join(self.bypass_domains)
+            shell_rc = shell["config"]
+            if not os.path.exists(shell_rc):
+                open(shell_rc, 'w').close()
 
-        if shell_name in ["BASH", "ZSH"]:
-            with open(shell_rc, 'a') as f:
-                f.write(f'export no_proxy="{bypass_domains_str}"\n')
-                f.write(f'export NO_PROXY="{bypass_domains_str}"\n')
-        elif shell_name == "FISH":
-            with open(shell_rc, 'a') as f:
-                f.write(f'set -x no_proxy "{bypass_domains_str}"\n')
-                f.write(f'set -x NO_PROXY "{bypass_domains_str}"\n')
+            bypass_domains_str = ",".join(self.bypass_domains)
 
-    def del_bypass_domains_env_var(self, shell_name, shell_rc):
-        if not os.path.exists(shell_rc):
-            return
+            if shell["name"] in ["BASH", "ZSH"]:
+                with open(shell_rc, 'a') as f:
+                    f.write(f'export no_proxy="{bypass_domains_str}"\n')
+                    f.write(f'export NO_PROXY="{bypass_domains_str}"\n')
+            elif shell["name"] == "FISH":
+                with open(shell_rc, 'a') as f:
+                    f.write(f'set -x no_proxy "{bypass_domains_str}"\n')
+                    f.write(f'set -x NO_PROXY "{bypass_domains_str}"\n')
 
-        with open(shell_rc, 'r') as f:
-            lines = f.readlines()
+    def unset_bypass_domains_env_var(self):
+        for shell in self.shells:
+            if not shell["installed"]:
+                continue
 
-        bypass_vars = [
-            "no_proxy",
-            "NO_PROXY"
-        ]
+            shell_rc = shell["config"]
+            if not os.path.exists(shell_rc):
+                continue
 
-        bypass_patterns_bash_zsh = [re.compile(rf'export {var}\=.*') for var in bypass_vars]
-        bypass_patterns_fish = [re.compile(rf'set -x {var} .*') for var in bypass_vars]
+            with open(shell_rc, 'r') as f:
+                lines = f.readlines()
 
-        if shell_name in ["BASH", "ZSH"]:
-            self.remove_matching_lines(lines, bypass_patterns_bash_zsh, shell_rc)
-        elif shell_name == "FISH":
-            self.remove_matching_lines(lines, bypass_patterns_fish, shell_rc)
+            bypass_vars = [
+                "no_proxy",
+                "NO_PROXY"
+            ]
+
+            bypass_patterns_bash_zsh = [re.compile(rf'export {var}\=.*') for var in bypass_vars]
+            bypass_patterns_fish = [re.compile(rf'set -x {var} .*') for var in bypass_vars]
+
+            if shell["name"] in ["BASH", "ZSH"]:
+                self.remove_matching_lines(lines, bypass_patterns_bash_zsh, shell_rc)
+            elif shell["name"] == "FISH":
+                self.remove_matching_lines(lines, bypass_patterns_fish, shell_rc)
